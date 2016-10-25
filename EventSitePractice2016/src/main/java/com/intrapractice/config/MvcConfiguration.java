572e30fd813282fc9a88de16d626e9803ac67623
Martin Parshorov <martin.parshorov@gmail.com>
2016-10-25 12:19:34 +0300

    Changed jdbc URL and added project schema for the DB

--- EventSitePractice2016/src/main/java/com/intrapractice/config/MvcConfiguration.java
+++ EventSitePractice2016/src/main/java/com/intrapractice/config/MvcConfiguration.java
@@ -2,6 +2,17 @@ package com.intrapractice.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
 import com.intrapractice.dao.CategoryDao;
 import com.intrapractice.dao.CategoryLikesDao;
 import com.intrapractice.dao.EventLikesDao;
@@ -15,17 +26,6 @@ import com.intrapractice.dao.impl.EventLikesDaoImpl;
 import com.intrapractice.dao.impl.EventParticipantsDaoImpl;
 import com.intrapractice.dao.impl.UserDaoImpl;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
 @Configuration
 @ComponentScan(basePackages="com.intrapractice")
 @EnableWebMvc
@@ -38,26 +38,25 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter{
 		resolver.setSuffix(".jsp");
 		return resolver;
 	}
-	
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
-	
+
 	@Bean
     public DataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/intrapractice");
+        dataSource.setUrl("jdbc:mysql://practicesqldb:3306/intrapractice");
         dataSource.setUsername("intrapractice");
         dataSource.setPassword("Ecp123ecp!");
-         
         return dataSource;
     }
-	
-	
+
+
 	@Bean
-    public JdbcTemplate jdbcTemplate() {    
+    public JdbcTemplate jdbcTemplate() {
         final JdbcTemplate jdbcTemplate = new JdbcTemplate();
 
         jdbcTemplate.setDataSource(getDataSource());
@@ -70,32 +69,32 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter{
     public UserDao getUserDAO() {
         return new UserDaoImpl();
     }
-    
+
     @Bean
     public EventsDao getEventsDAO(){
     	return new EventDaoImpl();
-    
+
     }
-    
+
     @Bean
     public EventParticipantsDao getEventsParticipantsDao() {
     	return new EventParticipantsDaoImpl();
     }
-    
-    @Bean 
+
+    @Bean
     public EventLikesDao getEventLikesDao() {
     	return new EventLikesDaoImpl();
     }
-    
+
     @Bean
     public CategoryDao getCategoryDao() {
     	return new CategoryDaoImpl();
-    } 
-    
+    }
+
     @Bean
     public CategoryLikesDao getCategoryLikesDao() {
     	return new CategoryLikesDaoImpl();
     }
-    
-    
+
+
 }
